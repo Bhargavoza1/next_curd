31ef67991a754b30734ef84c8feb04a07c3d0523
Bhargavoza1 <hexlok777@gmail.com>
2023-10-07 11:05:23 +0530

    basic crud done

--- src/app/api/addPost/route.ts
+++ src/app/api/addPost/route.ts
@@ -1,22 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {Request} from "next/dist/compiled/@edge-runtime/primitives";
-import clientPromise from "@/lib/mongodb";
 import connectMongoDB from "@/lib/mongodb";
 import Topic from "@/models/topic";
 import {NextResponse} from "next/server";
 
-type ResponseData = {
-    message: string
-}
-
 
 export async function POST(request ) {
-
         const {title, description} = await request.json();
         await connectMongoDB();
         await Topic.create({title, description});
         return  NextResponse.json({message:'connect'},{status:201});
-
 }
 
 export async function GET() {
@@ -33,11 +24,18 @@ export async function DELETE(request) {
 }
 
 export async function PUT(request) {
+        console.log(request.nextUrl)
         const id = request.nextUrl.searchParams.get("id");
+        const currentTopic =  await Topic.findById(id)
+
         const title = request.nextUrl.searchParams.get("title");
-        const description = request.nextUrl.searchParams.get("description");
+
+        const description = request.nextUrl.searchParams.get("description")
+
         await connectMongoDB();
-        const topics =  await Topic.findByIdAndUpdate(id,{title: title, description : description} );
+
+        const topics =  await Topic.findByIdAndUpdate(id,{title: title != null ? title : currentTopic.title
+                                                        , description : description != null ?  description : currentTopic.description} );
         return  NextResponse.json({topics},{status:201});
 }
 
